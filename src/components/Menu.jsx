317caf29f1b857ca05cb7817b4c8c9ef96be4157
Mohamed Abuzahda <mohamedabozahda21@gmail.com>
2025-07-18 16:57:41 +0300

    first commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,13 +3,13 @@ import React, { useState } from 'react';
 const menuItems = [
     { id: 1, name: "Margherita Pizza", price: 12.99, image: "../../public/assets/dcda3d76-687e-4fc8-b7f0-8dec95e37549.avif", category: "Pizza" },
     { id: 2, name: "Pepperoni Pizza", price: 14.99, image: "../../public/assets/f1b0be708793a9a42ee96e5f68bf0283.webp", category: "Pizza" },
-    { id: 3, name: "Caesar Salad", price: 8.99, image: "../../public/assets/images (1).jpg", category: "Salad" },
-    { id: 4, name: "Garlic Bread", price: 5.99, image: "../../public/assets/images.jpg", category: "Sides" },
-    { id: 5, name: "Garlic Bread", price: 5.99, image: "../../public/assets/Pizza-Hut-Charcuterie-Pizza-FT-BLOG0325-01-5dfdf7013950447a873985e19b0e2f67.jpg", category: "Sides" },
-    { id: 6, name: "Garlic Bread", price: 5.99, image: "../../public/assets/Pizza-Hut-The-Big-New-Yorker-Pizza-Taste-Test.-Allison-Robicelli-Resize-Recolor-Crop-DH-TOH-SOCIAL.jpg", category: "Sides" },
-    { id: 7, name: "Garlic Bread", price: 5.99, image: "../../public/assets/pizza-hut.jpg", category: "Sides" },
-    { id: 8, name: "Garlic Bread", price: 5.99, image: "../../public/assets/pizzahut.jpg", category: "Sides" },
-    { id: 9, name: "Garlic Bread", price: 5.99, image: "../../public/assets/images.jpg", category: "Sides" },
+    { id: 3, name: "Caesar Pizza", price: 8.99, image: "../../public/assets/images (1).jpg", category: "Salad" },
+    { id: 4, name: "Garlic Pizza", price: 7.99, image: "../../public/assets/images.jpg", category: "Sides" },
+    { id: 5, name: "Chicken Pizza", price: 5.99, image: "../../public/assets/Pizza-Hut-Charcuterie-Pizza-FT-BLOG0325-01-5dfdf7013950447a873985e19b0e2f67.jpg", category: "Sides" },
+    { id: 6, name: "Beef Pizza", price: 3.99, image: "../../public/assets/Pizza-Hut-The-Big-New-Yorker-Pizza-Taste-Test.-Allison-Robicelli-Resize-Recolor-Crop-DH-TOH-SOCIAL.jpg", category: "Sides" },
+    { id: 7, name: "Meat Pizza", price: 2.99, image: "../../public/assets/pizza-hut.jpg", category: "Sides" },
+    { id: 8, name: "Soses Pizza", price: 10.99, image: "../../public/assets/pizzahut.jpg", category: "Sides" },
+    { id: 9, name: "Swarm Pizza", price: 11, image: "../../public/assets/images.jpg", category: "Sides" },
 ];
 
 const Menu = ({ addToCart, addToWishlist, wishlist }) => {
